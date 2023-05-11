1baff717627b697d67db6226b1394f69d43eba03
Lujain <lujain_mohammed@outlook.sa>
2023-05-11 16:18:29 +0300

    Initial Commit

---
+++ Saudi Brands/Saudi_BrandsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Saudi_BrandsApp.swift
+//  Saudi Brands
+//
+//  Created by lujain mohammed on 21/10/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Saudi_BrandsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
